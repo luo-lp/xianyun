a1f080fc886194ec66a1d49c120426469934a0be
llp <875223115@qq.com>
2019-09-01 15:46:32 +0800

    项目初始化

---
+++ pages/tab.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span
+      v-for="(item, index) in ['新闻1', '新闻2', '新闻3']"
+      :key="index"
+      @click="active(index)"
+      :class="{hotYellow:index===newS}"
+    >{{ item }}</span>
+    <div v-show="newS===0">1111111111</div>
+    <div v-show="newS===1">2222222222</div>
+    <div v-show="newS===2">3333333333</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newS: 0
+    };
+  },
+  methods: {
+    active(index) {
+        this.newS=index
+    }
+  }
+};
+</script>
+
+<style>
+    .hotYellow{
+        background-color: yellowgreen;
+    }
+</style>
